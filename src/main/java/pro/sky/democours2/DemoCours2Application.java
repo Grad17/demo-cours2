9c3b3147e9683274593d606fdc9dc0889262d793
serge <sergei.g.d@gmail.ccom>
2022-09-16 18:47:13 +0700

    Initial commit

---
+++ src/main/java/pro/sky/democours2/DemoCours2Application.java
@@ -0,0 +1,13 @@
+package pro.sky.democours2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCours2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCours2Application.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/democours2/DemoCours2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.democours2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCours2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
